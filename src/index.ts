adf75f6359df2fc4504ae55ec3d6a46c74b38796
Juliano <julianofirme23@gmail.com>
2024-04-30 14:28:01 -0300

    feat: refactor and error handling

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,71 @@ import inquirer from 'inquirer';
 import { execSync } from 'child_process';
 
 function readPackageScripts() {
-  const packageJson = readPackageSync();
+  try {
+    const packageJson = readPackageSync();
 
-  if (!packageJson.scripts) {
-    console.log("No packages to read");
-    return;
-  };
+    if (!packageJson) {
+      console.log("No package.json found");
+      process.exit(1);
+    };
 
-  return Object.keys(packageJson.scripts);
+    if (!packageJson.scripts) {
+      console.log("No scripts to read");
+      process.exit(1);
+    };
+
+    return Object.keys(packageJson.scripts);
+  } catch (error) {
+    console.error("Error occurred while reading package.json:", error);
+    process.exit(1);
+  }
 }
 
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'script',
-      message: 'Select a script to run:',
-      choices: readPackageScripts(),
-    },
-  ])
-  .then((answers) => {
-    const scriptName = answers.script;
-    console.log(`Running script: ${scriptName}`);
-    const command = `npm run ${scriptName}`;
+function handleScriptExecution(scriptName: string) {
+  console.log(`Running script: ${scriptName}`);
+  const command = `npm run ${scriptName}`;
+  try {
     execSync(command, { stdio: 'inherit' });
-  })
-  .catch((error) => {
-    console.error('Error occurred:', error);
+  } catch (error) {
+    console.error(`Error occurred while running script ${scriptName}:`, error);
+  }
+}
+
+function handleExit() {
+  console.log("\n\nEsc key pressed. Exiting...");
+  process.exit(0);
+}
+
+function main() {
+  const scripts = readPackageScripts();
+
+  if (scripts.length === 0) {
+    console.log("No scripts found in package.json. Exiting...");
+    process.exit(1);
+  }
+
+  process.stdin.on("keypress", (ch, key) => {
+    if (key && key.name === "escape") {
+      handleExit();
+    }
   });
+
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'script',
+        message: 'Select a script to run:',
+        choices: scripts,
+      },
+    ])
+    .then((answers) => {
+      const scriptName = answers.script;
+      handleScriptExecution(scriptName);
+    })
+    .catch((error) => {
+      console.error('Error occurred:', error);
+    });
+}
+
+main();
